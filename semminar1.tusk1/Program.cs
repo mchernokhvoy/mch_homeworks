6fa790e5bbb06eba7d9526e427015d40f4a5730d
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-11-24 19:56:00 +0500

    tusk1 v.1

--- semminar1.tusk1/Program.cs
+++ semminar1.tusk1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// HOMEWORK 1.1
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse (Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse (Console.ReadLine());
+if (a < b)
+Console.Write("max = a, min = b");
+else
+Console.Write("max = b, min = a");
