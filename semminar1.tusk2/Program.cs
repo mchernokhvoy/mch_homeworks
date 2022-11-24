91f4d1a84add6d8a74a3eeccdb148609484dbe22
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-11-24 20:12:16 +0500

    tusk2 v.1

--- semminar1.tusk2/Program.cs
+++ semminar1.tusk2/Program.cs
@@ -1,2 +1,15 @@
 ﻿// HOMEWORK 1.2
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse (Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse (Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = int.Parse (Console.ReadLine());
+if (a > b && a > c)
+    Console.Write(a);
+if (b > a && b > c)
+    Console.Write(b);
+if (c > a && c > b)
+     Console.Write(c);
