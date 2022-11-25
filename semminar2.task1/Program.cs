9ac01a2a72be09971964b36c852d42b2f72782ed
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-11-25 15:50:52 +0500

    add change task2.1

--- semminar2.task1/Program.cs
+++ semminar2.task1/Program.cs
@@ -3,8 +3,8 @@
 
 Console.Clear();
 Console.Write("Введите число a: ");
-int a = int.Parse (Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 if (a >= 100 && a < 1000)
- Console.Write(a = (a - ((a / 100) * 100) - (a % 10)) / 10);
-else 
-Console.Write("Вы ввели не трёхзначное число, повторите попытку");
+    Console.Write(a = (a - ((a / 100) * 100) - (a % 10)) / 10);
+else
+    Console.Write("Вы ввели не трёхзначное число, повторите попытку");
