a413a856b20cf0b627739ea7d0d6dd29f7c31344
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-01 15:14:00 +0500

    added task 2.2

---
+++ semminar2.task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// HOMEWORK 2.2
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+while (a / 100 >=10 || a / 100 <=-10)
+{
+    a = a / 10;
+}
+if (a >= 100 && a < 1000)
+    Console.Write(a = (a - (a / 10) * 10));
+else if (a <= -100 && a > -1000)
+    Console.Write(a = -(a - (a / 10) * 10));
+else 
+    Console.Write("Вы ввели не трёхзначное число, повторите попытку");
