004f042785fe1b03ed5f87b0a5d8c29f629eb512
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-01 15:14:08 +0500

    added task 2.3

--- semminar2.task3/Program.cs
+++ semminar2.task3/Program.cs
@@ -1,12 +1,11 @@
 ﻿// HOMEWORK 2.3
 
-
 Console.Clear();
-Console.Write("Введите число a в диапозоне от 1 до 7, обозначающее день недели: ");
+Console.Write("Введите число a в диапозоне от 1 до 7: ");
 int a = int.Parse(Console.ReadLine());
 if (a >= 1 && a <= 5)
     Console.Write("будний день");
-if (a == 6 || a == 7)
+else if (a == 6 || a == 7)
     Console.Write("выходной день");
 else
     Console.Write("вы ввели некорректное число, укажите число от 1 до 7");
