b547564b8de60103c85b6e7cfd466d4403cf3468
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-05 17:50:26 +0500

    add task 3.1

---
+++ semminar3.task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// HOMEWORK 1.1
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse (Console.ReadLine());
+
+if (N > 0)
+    for (int i = 1 ; i <=N ; i++)
+    Console.Write ($"{i*i*i} ");
+
+else if (N < 0)
+    for (int i = -1 ; i >=N ; i--)
+    Console.Write ($"{i*i*i} ");
