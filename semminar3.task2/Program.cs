6e514c4c628a91bdb715b4694a7c876843099150
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-05 17:50:33 +0500

    add task 3.2

---
+++ semminar3.task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// HOMEWORK 3.2
+
+
+Console.Clear();
+Console.Write("Введите координату точки А: ");
+double xa = Convert.ToDouble (Console.ReadLine ());
+double ya = Convert.ToDouble (Console.ReadLine ());
+double za = Convert.ToDouble (Console.ReadLine ());
+
+Console.Write("Введите координату точки В: ");
+double xb = Convert.ToDouble (Console.ReadLine ());
+double yb = Convert.ToDouble (Console.ReadLine ());
+double zb = Convert.ToDouble (Console.ReadLine ());
+
+double result = Math.Sqrt (Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
+
+Console.WriteLine($"Расстояние между точками: {result}");
