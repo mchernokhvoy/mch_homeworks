5913fe1527f3b73ef7dcd930c868abee7522ad0a
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-08 18:01:00 +0500

    add task 4.2

---
+++ semminar4.task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// HOMEWORK 4.2
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int result = 0;
+while (n / 10 >= 1)
+{
+    result = result + (n % 10);
+    n = n / 10;
+}
+if (n / 10 >= 0)
+    result = result + n;
+Console.Write(result);
