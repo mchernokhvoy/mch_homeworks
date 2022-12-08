09226206073412e543eb07bd30b39526c7ac3f63
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-08 18:00:49 +0500

    add task 4.1

---
+++ semminar4.task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// HOMEWORK 4.1
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+int i = 1;
+int result = 1;
+while (i <= b)
+{
+    result = result * a;
+    i = i + 1;
+}
+Console.Write(result);
