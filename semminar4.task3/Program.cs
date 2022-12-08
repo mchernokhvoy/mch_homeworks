2eee3821127bdef6a2d0a2016654e36f5d272a4c
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-08 18:01:07 +0500

    add task 4.3

---
+++ semminar4.task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// HOMEWORK 4.3
+
+Console.Clear();
+Console.Write("Введите числа: ");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+int n4 = int.Parse(Console.ReadLine());
+int n5 = int.Parse(Console.ReadLine());
+int n6 = int.Parse(Console.ReadLine());
+int n7 = int.Parse(Console.ReadLine());
+int n8 = int.Parse(Console.ReadLine());
+int[] array = { n1, n2, n3, n4, n5, n6, n7, n8 };
+Console.WriteLine($"[{string.Join(", ", array)}]");
