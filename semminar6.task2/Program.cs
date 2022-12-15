3bf7bf816a710bd779593664a12072ec9774c7b9
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-15 21:05:59 +0500

    add task 6.2

---
+++ semminar6.task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// HOMEWORK 5.2
+
+Console.Clear();
+Console.Write("Введите значение K1: ");
+double K1 = Convert.ToDouble (Console.ReadLine());
+Console.Write("Введите значение B1: ");
+double B1 = Convert.ToDouble (Console.ReadLine());
+Console.Write("Введите значение K2: ");
+double K2 = Convert.ToDouble (Console.ReadLine());
+Console.Write("Введите значение B2: ");
+double B2 = Convert.ToDouble (Console.ReadLine());
+
+double x = (B2 - B1) / (K1 - K2);
+double y = K1 * x + B1;
+
+Console.WriteLine ($"({string.Join("; ", x, y )})");
