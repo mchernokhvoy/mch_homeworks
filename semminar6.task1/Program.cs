422f90e98570c9daf1286b7fa7cb18350a60a10e
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-15 21:05:50 +0500

    add task 6.1

---
+++ semminar6.task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// HOMEWORK 5.1
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+void ReleaseArray(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            result = result + 1;
+    }
+    Console.WriteLine($"Количество чётных элементов массива: {result}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
