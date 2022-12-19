641b07fd1794b05e89075b9f0b0b9ae12360038e
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-19 21:16:04 +0500

    add task2

---
+++ semminar7.task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// HOMEWORK 7.2
+
+Console.Clear();
+Console.Write("Введите позицию элемента в массиве: ");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int[,] matrix = new int[6, 6];
+InputMatrix(matrix);
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    if (x >= 0 && y >= 0 &&  x < matrix.GetLength(0) && y < matrix.GetLength(1))
+    {
+        Console.Write($"Заданный элемент массива = {matrix[x, y]} ");
+    }
+    else
+    {
+        Console.Write("Заданый элемент находится вне пределов массива");
+    }
+}
+Console.WriteLine();
+ReleaseMatrix(matrix);
