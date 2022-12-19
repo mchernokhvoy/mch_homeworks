c27a01ecac7b3fbe1f125a0560cd40146de18496
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-19 21:15:58 +0500

    add task1

---
+++ semminar7.task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// HOMEWORK 7.1
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (2 - (-2)) + (-2), 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] coord = Console.ReadLine().Split(" ");
+int row = int.Parse(coord[0]);
+int column = int.Parse(coord[1]);
+double[,] matrix = new double[row, column];
+InputMatrix(matrix);
