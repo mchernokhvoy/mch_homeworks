e23d1438970d302a7f0c27584b1d222d9cd89b0c
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-22 22:22:47 +0500

    add task8.2

--- semminar8.task2/Program.cs
+++ semminar8.task2/Program.cs
@@ -1,6 +1,5 @@
 ﻿// HOMEWORK 8.2
 
-
 void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -17,7 +16,7 @@ void InputMatrix(int[,] matrix)
 void ReleaseMatrix(int[,] matrix)
 {
     int minRow = 0;
-    int minSumString = 0;
+    // int minSumString = 0;
     for (int j = 0; j < matrix.GetLength(1); j++)
         minRow += matrix[0, j];
         // minSumString = 0;
@@ -32,16 +31,16 @@ void ReleaseMatrix(int[,] matrix)
                 minRow = sumRowElements;
                 // minSumString = i; 
     }
-    // Console.Write($"Строка с минимальной суммой элементов: {minSumString}\nСумма элементов строки = {minRow}");
+    // Console.Write($"Строка с минимальной суммой элементов: {i}\nСумма элементов строки = {minRow}");
     Console.Write($"Минимальная сумма элементов строки = {minRow}");
 }
 
 Console.Clear();
-Console.Write("Введите размер прямоугольного двумерного массива: ");
+Console.Write("Введите размер прямоугольной матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-while (size[0] == size[1])
+while (size[0] == size[1] || size[0] == 0|| size[1] == 0)
 {
-    Console.Write("Вы ошиблись!\n Введите размер прямоугольного двумерного массива: ");
+    Console.Write("Вы ввели размер некорректный размер\nВведите размер прямоугольной матрицы: ");
     size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 }
 int[,] matrix = new int[size[0], size[1]];
