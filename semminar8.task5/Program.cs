bc26e01711299452083ba661c5d94485dddbe160
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-22 21:25:42 +0500

    add task8.5

---
+++ semminar8.task5/Program.cs
@@ -0,0 +1,33 @@
+﻿// HOMEWORK 8.5
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int[,] matrix = new int[4, 4];
+int num = 1;
+int i = 0;
+int j = 0;
+while (num <= 4 * 4)
+{
+    matrix[i, j] = num;
+    if (i <= j + 1 && i + j < 4 - 1)
+        j++;
+    else if (i < j && i + j >= 4 - 1)
+        i++;
+    else if (i >= j && i + j > 4 - 1)
+        j--;
+    else
+        i--;
+    num++;
+}
+PrintMatrix(matrix);
