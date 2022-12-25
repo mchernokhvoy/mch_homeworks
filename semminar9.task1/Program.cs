6bb478cccf48f449fe40dc1f3458759ea4700881
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-26 01:58:48 +0500

    add task 9.1

---
+++ semminar9.task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// HOMEWORK 9.1
+
+
+int sum (int n, int m)
+{
+    if (n == m)
+        return m;
+    return sum (n + 1, m) + n;
+}
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Сумма = ");
+Console.WriteLine(sum(n, m));
